c94f5c32836a4ce2df49e73c73d1c97784e015e5
Sayuri Bose <sayuribose@gmail.com>
2025-08-08 14:36:17 -0400

    copy pasted

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -1,64 +1,49 @@
+// ContentView.swift
+// ToDoList45
 //
-//  ContentView.swift
-//  ToDoList
+// Created by Scholar on 8/8/25.
 //
-//  Created by Scholar on 8/8/25.
-//
-
 import SwiftUI
 import SwiftData
-
 struct ContentView: View {
-    
-    @State private var showNewTask = false
-    @Query var ToDos: [ToDoItem]
-    
-    var body: some View {
-        VStack {
-            HStack {
-                Text("To-Do List")
-                    .font(.system(size: 40))
-                    .fontWeight(.black)
-                Spacer()
-                Button {
-                    withAnimation {
-                        showNewTask = true
-                    }
-                    showNewTask = true
-                    
-                }label: {
-                    Text("+")
-                        .font(.title)
-                        .fontWeight(.bold)
-                }
-            }//hstack
-            
-            Spacer()
-            
-            List{//display lists of tasks
-                ForEach(ToDos) { toDoItem in
-                    if ToDoItem.isImportant {
-                        Text("‼️" + ToDoItem.title)
-                    } else {
-                        Text(ToDoItem.title)
-                    }
-                    
-                }
-            }
-            .listStyle(.plain)
-        
-            if showNewTask {
-                NewToDo(showNewTask: $showNewTask, ToDoItem: ToDoItem(title: "", isImportant: false))
-            }
-            
-        }//vstack
-        
-        
-        .padding()
-    }//someview
-}//contentview
-
+  @State private var showNewTask = false
+  @Query var toDos: [ToDoItem]
+  var body: some View {
+    VStack {
+      HStack{
+        Text("To-Do List")
+          .font(.system(size: 40))
+          .fontWeight(.bold)
+        Spacer()//push left or right
+        Button{
+          withAnimation{
+            showNewTask = true
+          }
+        } label: {
+          Text("+")
+            .font(.title)
+            .fontWeight(.bold)
+        }
+      }//end of the HStack
+      .padding()
+      Spacer()//push up
+      List{//Displays list of tasks
+        ForEach (toDos) { toDoItem in
+          if toDoItem.isImportant {
+            Text(":bangbang:" + toDoItem.title)
+          } else {
+            Text(toDoItem.title)
+          }
+        }
+      }
+      .listStyle(.plain)
+    }//end of the VStack
+    if showNewTask {
+      NewToDo(showNewTask: $showNewTask, toDoItem: ToDoItem(title: "", isImportant: false))
+    }
+  }
+}
 #Preview {
-    ContentView()
-        .modelContainer(for: ToDoItem.self, inMemory: true)
+  ContentView()
+    .modelContainer(for: ToDoItem.self, inMemory: true)
 }
--- ToDoList/NewToDo.swift
+++ ToDoList/NewToDo.swift
@@ -1,55 +1,54 @@
+//NewToDo.swift
+// ToDoList45
 //
-//  NewToDo.swift
-//  ToDoList
+// Created by Scholar on 8/8/25.
 //
-//  Created by Scholar on 8/8/25.
-//
-
 import SwiftUI
-
 struct NewToDo: View {
-    @Binding var showNewTask: Bool
-    @Bindable var ToDoItem: ToDoItem
-    @Environment(\.modelContext) var modelContext
-    
-    var body: some View {
-        
-        
-        
-        VStack {
-            Text("Task Title:")
-                .fontWeight(.bold)
-            TextField("Enter the task description...", text: $ToDoItem.title)
-                .padding()
-                .background(Color(.systemGroupedBackground))
-                .cornerRadius(15)
-                .padding()
-            
-            Toggle(isOn: $ToDoItem.isImportant) {
-                Text("Is it important?")
-            }
-            Button {
-                addToDo()
-                showNewTask = false
-                
-            } label: {
-                Text("Save")
-            }
-        }//end of vstack
+  @Binding var showNewTask: Bool
+  @Bindable var toDoItem: ToDoItem
+  @Environment(\.modelContext) var modelContext
+  var body: some View {
+    VStack {
+      Text("Task Title:")
+        .font(.title)
+        .fontWeight(.bold)
+      TextField("Enter the task description", text: $toDoItem.title)
         .padding()
-    
-    func addToDo() {
-        let toDo = ToDoList.ToDoItem(title: ToDoItem.title, isImportant: ToDoItem.isImportant)
-            modelContext.insert(toDo)
-        }
-        }
-    
-    
-        
-    }
-
-
-#Preview {
-    NewToDo(showNewTask: .constant(false), ToDoItem:
-                ToDoItem: "", isImportant: false))
+        .background(Color(.systemGroupedBackground))
+        .cornerRadius(15)
+        .padding()
+      Toggle(isOn: $toDoItem.isImportant) {
+        Text("Is it important?")
+      }
+      Button {
+        addToDo()
+        showNewTask = false
+      } label: {
+        Text("Save")
+      }
+       
+    }//end of VStack
+    .padding()
+     
+  }
+     
+  func addToDo() {
+    let toDo = ToDoItem(title: toDoItem.title, isImportant: toDoItem.isImportant)
+      modelContext.insert(toDo)
+  }
 }
+#Preview {
+  NewToDo(showNewTask: .constant(false), toDoItem: ToDoItem(title: "", isImportant: false))
+}
+
+
+
+
+
+
+
+
+
+
+
