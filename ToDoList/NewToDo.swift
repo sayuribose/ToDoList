4dcc9e444e7e89559e912caf169b2f2e26f03b50
Sayuri Bose <sayuribose@gmail.com>
2025-08-08 13:25:45 -0400

    added vstack button toggle and text field

--- ToDoList/NewToDo.swift
+++ ToDoList/NewToDo.swift
@@ -9,9 +9,31 @@ import SwiftUI
 
 struct NewToDo: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+        VStack {
+            Text("Task Title:")
+                .fontWeight(.bold)
+            TextField("Enter the task description...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .padding()
+                .background(Color(.systemGroupedBackground))
+                .cornerRadius(15)
+                .padding()
+            
+            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                Text("Is it important?")
+            }
+            Button {
+                
+            } label: {
+                Text("Save")
+            }
+        }//end of vstack
+        .padding()
+        }
+        
     }
-}
+
 
 #Preview {
     NewToDo()
